eaab230c6dcdd4f4cdc4994f3d766e9a368e5836
Aly <batcha@hartwick.edu>
2019-10-10 09:20:46 -0400

    Update

---
+++ EX31 Activity.py
@@ -0,0 +1,13 @@
+# Aly Batch
+# Making Decisions
+# 10.10.19
+
+print("""You enter a dark room with two doors.
+Do you go through door #1 or dor #2?""")
+
+door = input("> ")
+
+if door == "1":
+    print("There's a giant bear eating a cheese cake.")
+    print("What do you do?")
+    print("1. Take the cake.")
